f8ee889029106aaa6d22835f9d95b6b33d5ee836
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-01-05 22:43:23 -0600

    Initial Commit

---
+++ Sleep-Assist/Sleep_AssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_AssistApp.swift
+//  Sleep-Assist
+//
+//  Created by Jared Thompkins on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_AssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
